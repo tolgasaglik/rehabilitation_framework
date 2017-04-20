733285c7db7fc7d43dabb8cb007c09eb2d6e0c73
Trinity92 <gomesf.leandro@gmail.com>
2017-04-20 15:28:01 +0200

    Framework now reads encouragement sentences from file and tells themwhen appropriate (command line arguments temporarily disabled for this purpose)

--- src/game2.py
+++ src/game2.py
@@ -4,7 +4,10 @@ import cv2
 import numpy as np
 import sys
 import argparse
-
+import roslib; roslib.load_manifest('reha_game')
+import rospy
+import rospkg
+from std_msgs.msg import String
 from os.path import expanduser
 import os
 import shlex, subprocess
@@ -17,47 +20,113 @@ import shlex, subprocess
 #       C         |      B
 #                 |y=0.1
 
-# argument parsing
-parser = argparse.ArgumentParser(description='Reads video footage from a camera or a video file, and performs the Rehazenter exercise on it.')
-parser.add_argument('--width', type=int, dest="width", help='width of the camera window')
-parser.add_argument('--height', type=int, dest="height", help='height of the camera window')
-parser.add_argument('--path-to-video', '-p', type=str, dest="path_to_video", help='file path to the video file (default: webcam)')
-parser.add_argument('--path-to-samples-folder', '-f', type=str, dest="path_to_samples_folder", help='path to the folder where the object\'s image samples are stored')
-parser.add_argument('--path-to-output-video', '-o', type=str, dest="path_to_output_video", help='path to the output video that contains the modified input video with the detected object contours')
-args = parser.parse_args()
+class EncouragerUnit(object):
+	repetitions = 0
+	repetitions_limit = 0
+	pub = None
+	sentences = []
+	ENC_SENT_FILELOC = "/config/encouragement_sentences.txt"
+
+	def __init__(self, repetitions_limit):
+		self.repetitions_limit = repetitions_limit
+		rospy.init_node('reha_game')
+		self.pub = rospy.Publisher('/robot/voice', String)
+		self.load_sentences()
+
+	def load_sentences(self):
+		rospack = rospkg.RosPack()
+		reha_game_pkg_path = rospack.get_path('reha_game')
+		sentences_file = open(reha_game_pkg_path + self.ENC_SENT_FILELOC, "r")
+		self.sentences = sentences_file.readlines()
+		sentences_file.close()
+
+	def incRepCounter(self):
+		self.repetitions += 1
+		self.pub.publish(str(self.repetitions))
+		if self.repetitions == self.repetitions_limit/2:
+			self.pub.publish(self.sentences[0])
+		elif self.repetitions == self.repetitions_limit-1:
+			self.pub.publish(self.sentences[1])
+		elif self.repetitions == self.repetitions_limit:
+			self.pub.publish(self.sentences[2])
+	
+
+def parse_arguments(camera_width, camera_height, path_to_video, path_to_output_video, color):
+	# argument parsing
+	parser = argparse.ArgumentParser(description='Reads video footage from a camera or a video file, and performs the Rehazenter exercise on it.')
+	parser.add_argument('--width', type=int, dest="width", help='width of the camera window')
+	parser.add_argument('--height', type=int, dest="height", help='height of the camera window')
+	parser.add_argument('--path-to-video', '-p', type=str, dest="path_to_video", help='file path to the video file (default: webcam)')
+	#parser.add_argument('--path-to-samples-folder', '-f', type=str, dest="arg_path_to_samples_folder", help='path to the folder where the object\'s image samples are stored')
+	parser.add_argument('--path-to-output-video', '-o', type=str, dest="path_to_output_video", help='path to the output video that contains the modified input video with the detected object contours')
+	parser.add_argument('--color', '-c', type=str, dest="color", help='the color of the object that should be tracked (default=yellow)')
+	args = parser.parse_args()
+
+	# checking color argument
+	color = "yellow"
+	if args.color != "yellow" or args.color != "blue" or args.color != "black" or args.color:
+		print "Unrecognized color! Setting color back to default (yellow)."
+	else:
+		color = args.color
+
+	# checking camera dimensions (TODO: check if correct!)
+	camera_width = args.width
+	camera_width = args.height
+	
+	# TODO: add remaining checks for path params
 
 
-# HSV color thresholds for YELLOW
-THRESHOLD_LOW = np.array([15,210,20], dtype=np.uint8)
-THRESHOLD_HIGH = np.array([35,255,255], dtype=np.uint8)
 
-# HSV color thresholds for BLUE
-#THRESHOLD_LOW = np.array([110,50,50], dtype=np.uint8)
-#THRESHOLD_HIGH = np.array([130,255,255], dtype=np.uint8)
+# ************************************************************************************
+# *********************************** MAIN PROGRAM ***********************************
+# ************************************************************************************
 
-# HSV color thresholds for BLACK (not recommended for usage due to huge amounts of noise)
-#THRESHOLD_LOW = np.array([60,15,0], dtype=np.uint8)
-#THRESHOLD_HIGH = np.array([105,170,110], dtype=np.uint8)
+# set trivial variables and parse arguments
+CAMERA_WIDTH = 640
+CAMERA_HEIGHT = 480
+PATH_TO_VIDEO = ""
+#PATH_TO_SAMPLES_FOLDER = ""
+PATH_TO_OUTPUT_VIDEO = ""
+COLOR = "yellow"
+#parse_arguments(CAMERA_WIDTH, CAMERA_HEIGHT, PATH_TO_VIDEO, PATH_TO_OUTPUT_VIDEO, COLOR)
 
-# get color automatically from images, if argument given
-if args.path_to_samples_folder != None:
-	if os.path.exists(args.path_to_samples_folder) == False :
-		print "The specified path to the samples folder does not exist! Aborting..."
-		sys.exit()
-	# loop through image files
-	for img_fn in os.listdir(args.path_to_samples_folder):
-		if os.path.isfile(img_fn) and img_fn.lower().endswith(('.png', '.jpg', '.jpeg')):
-			# TODO: go through pixels in image and store max/min thresholds of biggest blob
-			img = cv2.imread(img_fn, cv2.IMREAD_COLOR)
-	      		print (fn)
+# default thresholds (yellow)
+# **************************************************
+# argument parsing (the ROS way)
+#if rospy.has_param(self._Cascade_Classifier):
+#      cascPath = rospy.get_param(self._Cascade_Classifier)
+#    else:
+#      rospy.logwarn("CAscade parameters need to be set to start recognizer.")
+#      return
 
-# Webcam parameters (your desired resolution)
-CAMERA_WIDTH = 320
-CAMERA_HEIGHT = 240
-if args.width != None:
-	CAMERA_WIDTH = args.width
-if args.height != None:
-	CAMERA_HEIGHT = args.height
+
+# set HSV color thresholds
+if COLOR == "blue":
+	# HSV color thresholds for BLUE
+	THRESHOLD_LOW = np.array([110,50,50], dtype=np.uint8)
+	THRESHOLD_HIGH = np.array([130,255,255], dtype=np.uint8)
+elif COLOR == "black":
+	# HSV color thresholds for BLACK (not recommended for usage due to huge amounts of noise)
+	THRESHOLD_LOW = np.array([60,15,0], dtype=np.uint8)
+	THRESHOLD_HIGH = np.array([105,170,110], dtype=np.uint8)
+else:
+	# HSV color thresholds for YELLOW (default)
+	THRESHOLD_LOW = np.array([15,210,20], dtype=np.uint8)
+	THRESHOLD_HIGH = np.array([35,255,255], dtype=np.uint8)
+print "Using color " + COLOR + "."
+
+# TODO: get color automatically from images, if argument given
+#if args.path_to_samples_folder != None:
+#	if os.path.exists(args.path_to_samples_folder) == False :
+#		print "The specified path to the samples folder does not exist! Aborting..."
+#		sys.exit()
+#	# loop through image files
+#	for img_fn in os.listdir(args.path_to_samples_folder):
+#		if os.path.isfile(img_fn) and img_fn.lower().endswith(('.png', '.jpg', '.jpeg')):
+#			# TODO: go through pixels in image and store max/min thresholds of biggest blob
+#			img = cv2.imread(img_fn, cv2.IMREAD_COLOR)
+#	     		print (fn)
+
 
 # Minimum required radius of enclosing circle of contour
 MIN_RADIUS = 2
@@ -67,10 +136,10 @@ HAND_MOV_THRESHOLD_LEFT = CAMERA_WIDTH / 4
 HAND_MOV_THRESHOLD_RIGHT = HAND_MOV_THRESHOLD_LEFT * 3
 
 # Initialize camera and get actual resolution
-if args.path_to_video == None:
+if PATH_TO_VIDEO == "":
 	cap = cv2.VideoCapture(0)
 else:
-	cap = cv2.VideoCapture(args.path_to_video)
+	cap = cv2.VideoCapture(PATH_TO_VIDEO)
 
 # check if camera/video file was opened successfully
 if cap.isOpened() == False :
@@ -79,16 +148,13 @@ if cap.isOpened() == False :
 
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, CAMERA_WIDTH)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, CAMERA_HEIGHT)
-camWidth = cap.get(cv2.CAP_PROP_FRAME_WIDTH)
-camHeight = cap.get(cv2.CAP_PROP_FRAME_HEIGHT)
-#print str(camWidth)
-#print str(camHeight)
 
-# define some trivial variables for our main loop
+# define some variables needed for our main loop
 last_moment = 0        
 moment_status = 0  
 last_moment_status = 0
 loop = 0
+has_motivated = False
 
 # variable that tracks direction in which patient should currently move his hand
 hand_is_moving_right = True
@@ -96,13 +162,17 @@ hand_is_moving_right = True
 # stores number of exercise repetitions done so far
 repetitions = 0
 
+# read encouragement sentences from file
+
 # Define the codec and create VideoWriter object
 fourcc = cv2.VideoWriter_fourcc(*'mp4v') # Be sure to use the lower case
-if args.path_to_output_video == None:
+if PATH_TO_OUTPUT_VIDEO == "":
 	out = cv2.VideoWriter('output.mp4v', fourcc, 20.0, (568, 516))
 else:
-	out = cv2.VideoWriter(args.path_to_output_video, fourcc, 20.0, (568, 516))
+	out = cv2.VideoWriter(PATH_TO_OUTPUT_VIDEO, fourcc, 20.0, (568, 516))
 
+# launch ROS communication handler
+encourager = EncouragerUnit(10)
 print "Camera dimensions: " +  str(CAMERA_WIDTH) + " x " + str(CAMERA_HEIGHT)
 print "Press the \"q\" key to quit."
 
@@ -165,10 +235,8 @@ while True:
 	if center != None :
 		if hand_is_moving_right == True and center[0] > HAND_MOV_THRESHOLD_RIGHT :
 			hand_is_moving_right = False
-			repetitions += 1
-			print "Current number of repetitions done: " + str(repetitions)
-			#subprocess.call(["rosrun", "sound_play say.py \"yes\""])
-			os.system('rosrun sound_play say.py "'+ str(repetitions) + '"')
+			encourager.incRepCounter()
+			print "Current number of repetitions done: " + str(encourager.repetitions)
     		elif hand_is_moving_right == False and center[0] < HAND_MOV_THRESHOLD_LEFT :
 			hand_is_moving_right = True
 
@@ -180,7 +248,7 @@ while True:
 
 	# Draw a green circle around the largest enclosed contour
 	if center != None:
-		cv2.circle(img, center, int(round(radius)),  np.array([0,255,0]))
+		cv2.circle(img, center, int(round(radius)), np.array([0,255,0]))
 
 	font = cv2.FONT_HERSHEY_SIMPLEX
 	cv2.putText(img,str(loop),(10,50), font, 1,(0,0,255),2)
