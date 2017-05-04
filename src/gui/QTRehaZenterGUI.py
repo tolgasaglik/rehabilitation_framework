79b57fec120334bd9a080f1b657321bd888132bc
Trinity92 <gomesf.leandro@gmail.com>
2017-05-04 18:11:17 +0200

    Continued work on GUI, some file renaming for better overview

--- src/gui/QTRehaZenterGUI.py
+++ src/gui/QTRehaZenterGUI.py
@@ -7,6 +7,13 @@
 # WARNING! All changes made in this file will be lost!
 
 from PyQt4 import QtCore, QtGui
+import os,sys,inspect
+# include parent "src" directory to sys.path, otherwise import won't work
+# (source: http://stackoverflow.com/questions/714063/importing-modules-from-parent-folder)
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir) 
+import Exercise1
 
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
@@ -21,6 +28,23 @@ try:
 except AttributeError:
     def _translate(context, text, disambig):
         return QtGui.QApplication.translate(context, text, disambig)
+
+# The new Stream Object which replaces the default stream associated with sys.stdout
+# This object just puts data in a queue!
+#class WriteStream(object):
+#    def __init__(self, queue):
+#        self.queue = queue
+#
+#    def write(self, text):
+#        self.queue.put(text)
+
+
+# solution below taken from: http://stackoverflow.com/questions/8356336/how-to-capture-output-of-pythons-interpreter-and-show-in-a-text-widget
+class EmittingStream(QtCore.QObject):
+    textWritten = QtCore.pyqtSignal(str)
+
+    def write(self, text):
+        self.textWritten.emit(str(text))
 
 class Ui_QTRehaZenter(object):
     def setupUi(self, QTRehaZenter):
@@ -67,6 +91,7 @@ class Ui_QTRehaZenter(object):
         self.btnStop.setMinimumSize(QtCore.QSize(200, 120))
         self.btnStop.setCheckable(False)
         self.btnStop.setObjectName(_fromUtf8("btnStop"))
+	self.btnStop.setEnabled(False)
         self.horizontalLayout.addWidget(self.btnStop)
         self.gridLayout.addLayout(self.horizontalLayout, 6, 0, 1, 2)
         self.lblROSConsole = QtGui.QLabel(self.centralwidget)
@@ -87,6 +112,7 @@ class Ui_QTRehaZenter(object):
         self.btnExercise4.setObjectName(_fromUtf8("btnExercise4"))
         self.gridLayout.addWidget(self.btnExercise4, 3, 1, 1, 1)
         self.btnExercise2 = QtGui.QPushButton(self.centralwidget)
+        self.btnExercise2.setEnabled(True)
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.MinimumExpanding, QtGui.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
@@ -95,15 +121,6 @@ class Ui_QTRehaZenter(object):
         self.btnExercise2.setMinimumSize(QtCore.QSize(0, 200))
         self.btnExercise2.setObjectName(_fromUtf8("btnExercise2"))
         self.gridLayout.addWidget(self.btnExercise2, 2, 1, 1, 1)
-        self.txtViewROSConsole = QtGui.QTextBrowser(self.centralwidget)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.txtViewROSConsole.sizePolicy().hasHeightForWidth())
-        self.txtViewROSConsole.setSizePolicy(sizePolicy)
-        self.txtViewROSConsole.setMinimumSize(QtCore.QSize(751, 400))
-        self.txtViewROSConsole.setObjectName(_fromUtf8("txtViewROSConsole"))
-        self.gridLayout.addWidget(self.txtViewROSConsole, 9, 0, 1, 2)
         self.line_2 = QtGui.QFrame(self.centralwidget)
         sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Maximum)
         sizePolicy.setHorizontalStretch(0)
@@ -132,6 +149,15 @@ class Ui_QTRehaZenter(object):
         self.btnExercise1.setMinimumSize(QtCore.QSize(0, 200))
         self.btnExercise1.setObjectName(_fromUtf8("btnExercise1"))
         self.gridLayout.addWidget(self.btnExercise1, 2, 0, 1, 1)
+        self.txtViewROSConsole = QtGui.QPlainTextEdit(self.centralwidget)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.txtViewROSConsole.sizePolicy().hasHeightForWidth())
+        self.txtViewROSConsole.setSizePolicy(sizePolicy)
+        self.txtViewROSConsole.setMinimumSize(QtCore.QSize(751, 400))
+        self.txtViewROSConsole.setObjectName(_fromUtf8("txtViewROSConsole"))
+        self.gridLayout.addWidget(self.txtViewROSConsole, 9, 0, 1, 2)
         QTRehaZenter.setCentralWidget(self.centralwidget)
         self.menubar = QtGui.QMenuBar(QTRehaZenter)
         self.menubar.setGeometry(QtCore.QRect(0, 0, 957, 35))
@@ -166,6 +192,21 @@ class Ui_QTRehaZenter(object):
         self.retranslateUi(QTRehaZenter)
         QtCore.QMetaObject.connectSlotsByName(QTRehaZenter)
 
+	# connect functions to buttons
+	self.btnExercise3.clicked.connect(self.btnExercise3Clicked)
+	self.btnExercise4.clicked.connect(self.btnExercise4Clicked)
+	self.btnExercise2.clicked.connect(self.btnExercise2Clicked)
+	self.btnExercise1.clicked.connect(self.btnExercise1Clicked)
+	self.btnBegin.clicked.connect(self.btnBeginClicked)
+	self.btnStop.clicked.connect(self.btnStopClicked)
+
+	#stdout = WriteStream(self)
+	#stdout.outputWritten.connect(self.handleROSOutput)
+	#sys.stdout = EmittingStream(textWritten=self.normalOutputWritten)
+
+    #def normalOutputWritten(self, text):
+    #	self.txtViewROSConsole.appendPlainText(text)
+
     def retranslateUi(self, QTRehaZenter):
         QTRehaZenter.setWindowTitle(_translate("QTRehaZenter", "MainWindow", None))
         self.btnBegin.setText(_translate("QTRehaZenter", "Begin!", None))
@@ -183,6 +224,81 @@ class Ui_QTRehaZenter(object):
         self.actionAbout.setText(_translate("QTRehaZenter", "About...", None))
         self.actionQuit.setText(_translate("QTRehaZenter", "Quit", None))
 
+    def __del__(self):
+    	# Restore sys.stdout
+    	sys.stdout = sys.__stdout__
+
+    # *******************************************************************************************
+    # *************************  connector functions for the UI buttons  ************************
+    # *******************************************************************************************
+    selectedButton = 0
+    def btnExercise1Clicked(self):
+	# enable all other buttons except the one for exercise 1
+	self.btnExercise1.setEnabled(False)
+	self.btnExercise2.setEnabled(True)
+	self.btnExercise3.setEnabled(True)
+	self.btnExercise4.setEnabled(True)
+	self.selectedButton = 1
+	self.txtViewROSConsole.appendPlainText("Exercise 1 selected.")
+	
+    def btnExercise2Clicked(self):
+	# enable all other buttons except the one for exercise 2
+	self.btnExercise1.setEnabled(True)
+	self.btnExercise2.setEnabled(False)
+	self.btnExercise3.setEnabled(True)
+	self.btnExercise4.setEnabled(True)
+	self.selectedButton = 2
+	self.txtViewROSConsole.appendPlainText("Exercise 2 selected.")
+
+    def btnExercise3Clicked(self):
+	# enable all other buttons except the one for exercise 3
+	self.btnExercise1.setEnabled(True)
+	self.btnExercise2.setEnabled(True)
+	self.btnExercise3.setEnabled(False)
+	self.btnExercise4.setEnabled(True)
+	self.selectedButton = 3
+	self.txtViewROSConsole.appendPlainText("Exercise 3 selected.")
+
+    def btnExercise4Clicked(self):
+	# enable all other buttons except the one for exercise 4
+	self.btnExercise1.setEnabled(True)
+	self.btnExercise2.setEnabled(True)
+	self.btnExercise3.setEnabled(True)
+	self.btnExercise4.setEnabled(False)
+	self.selectedButton = 4
+	self.txtViewROSConsole.appendPlainText("Exercise 4 selected.")
+
+    def btnBeginClicked(self):
+	# disable all other buttons while the chosen exercise is running
+	self.btnExercise1.setEnabled(False)
+	self.btnExercise2.setEnabled(False)
+	self.btnExercise3.setEnabled(False)
+	self.btnExercise4.setEnabled(False)
+	self.btnBegin.setEnabled(False)
+	self.btnStop.setEnabled(True)
+	self.txtViewROSConsole.appendPlainText("Exercise " + str(self.selectedButton) + ": STARTED!")
+	if self.selectedButton == 1:
+		argv = ["--width", "640", "--height", "480", "--color", "yellow", "--calibrate"]
+		ex1game = Exercise1.Exercise1Thread(argv)
+		ex1game.start()
+
+	# TODO: run ROS launch file and print ROS log stuff on GUI
+
+    def btnStopClicked(self):
+	# enable all other buttons again
+	self.btnExercise1.setEnabled(True)
+	self.btnExercise2.setEnabled(True)
+	self.btnExercise3.setEnabled(True)
+	self.btnExercise4.setEnabled(True)
+	self.btnBegin.setEnabled(True)
+	self.btnStop.setEnabled(False)
+	selectedButton = 0
+	self.txtViewROSConsole.appendPlainText("Exercise " + str(self.selectedButton) + ": STOPPED!")
+
+    def handleROSOutput(self, ):
+	self.txtViewROSConsole.appendPlainText()
+
+    # *******************************************************************************************
 
 if __name__ == "__main__":
     import sys
